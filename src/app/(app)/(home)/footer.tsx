d77c262b24b39940ac1585ba0a12f2fa9d26e6ec
Omar Acuña <13097@ulsachihuahua.edu.mx>
2025-06-20 23:13:32 -0600

    add Payload CMS with MongoDB database connection

--- src/app/(app)/(home)/footer.tsx
+++ src/app/(app)/(home)/footer.tsx
@@ -1,6 +1,7 @@
 export const Footer = () => {
   return (
-    <footer className="flex border-t justify-between font-medium p-4">
+    <footer className="flex border-t justify-center font-medium p-4">
+      {" "}
       <div>
         <p>Swapi &copy; {new Date().getFullYear()}</p>
       </div>
